84b115dce0f763312f4cfd07c320a1785be9764a
Yuni Cho <jjoyuni@gmail.com>
2022-06-22 17:34:22 +0900

    Initialise project

---
+++ preporojecttest/src/main/java/com/example/preporojecttest/PreporojecttestApplication.java
@@ -0,0 +1,13 @@
+package com.example.preporojecttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreporojecttestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreporojecttestApplication.class, args);
+	}
+
+}
---
+++ preporojecttest/src/test/java/com/example/preporojecttest/PreporojecttestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.preporojecttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreporojecttestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
